93c632ca39237062a05edfbc12cc2980ed179841
tobinio <Tobias.frischmann1@gmail.com>
2024-03-24 15:52:14 +0100

    fix preset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	preset: "bun",
 	devtools: { enabled: true },
 	nitro: {
+		preset: "bun",
 		experimental: {
 			database: true,
 			// tasks: true,
