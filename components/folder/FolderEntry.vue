56505ec008b9d7561db136c1a176cea27939083d
tobinio <Tobias.frischmann1@gmail.com>
2024-06-11 19:42:02 +0200

    open/close sub-folders

--- components/folder/FolderEntry.vue
+++ components/folder/FolderEntry.vue
@@ -8,6 +8,7 @@ const { data, refresh } = useFetch("/api/folder/", {
 		folder_id: props.folder.id,
 	},
 });
+const showSubFolders = ref(false);
 
 const dirname = ref("");
 
@@ -25,43 +26,55 @@ async function onCreateNewDir() {
 </script>
 
 <template>
-  <div>
-    {{ folder.name}}
+  <div id="main">
+    <div id="header">
+      {{ folder.name}}
+      <input v-model="showSubFolders" type="checkbox">
+    </div>
 
-    <div id="sub">
+    <div id="sub" v-if="showSubFolders">
       <div id="line">
       </div>
       <div>
         <FolderEntry v-for="sub_folder in data" :folder="sub_folder"/>
+        <form id="folderInput" @submit.prevent="onCreateNewDir">
+          <button class="myIcon" id="icon" type="submit">
+            <icon name="basil:folder-plus-outline" size="24" color="var(--white)"/>
+          </button>
+          <input v-model="dirname" type="text" name="dirName" placeholder="Folder Name">
+        </form>
       </div>
     </div>
-
-    <form id="folderInput" @submit.prevent="onCreateNewDir">
-      <button class="myIcon" id="icon" type="submit">
-        <icon name="basil:folder-plus-outline" size="24" color="var(--white)"/>
-      </button>
-      <input v-model="dirname" type="text" name="dirName" placeholder="Folder Name">
-    </form>
   </div>
 </template>
 
 <style scoped>
-#sub{
-  display: flex;
+#main {
+  margin-top: 5px;
+  margin-bottom: 5px;
 
-  #line{
-    background-color: black;
-
-    width: 1px;
-    margin: 5px;
+  #header{
+    display: flex;
+    justify-content: space-between;
   }
-}
 
-#folderInput{
-  display: flex;
+  #sub{
+    display: flex;
 
-  input{
-    width: 100%;
+    #line{
+      background-color: black;
+
+      width: 1px;
+      margin: 5px;
+    }
+
+    #folderInput{
+      display: flex;
+
+      input{
+        width: 100%;
+      }
+    }
   }
 }
 </style>
