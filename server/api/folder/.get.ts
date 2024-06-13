39d700876bdc3723e44e79364e056d8e514f73ca
to_binio <tobias.frischmann1@gmail.com>
2024-06-13 09:58:12 +0200

    biome lints

--- server/api/folder/.get.ts
+++ server/api/folder/.get.ts
@@ -1,24 +1,24 @@
-import type {Folder} from "~/utils/types/folder";
+import type { Folder } from "~/utils/types/folder";
 
 export default defineEventHandler(async (event): Promise<Folder[]> => {
-    const query = getQuery<{ folder_id: string }>(event);
-    const database = useDatabase();
+	const query = getQuery<{ folder_id: string }>(event);
+	const database = useDatabase();
 
-    const result = await database.sql`
+	const result = await database.sql`
         SELECT name, id
         FROM folder
         WHERE folder.parent_folder = ${query.folder_id}
     `;
 
-    if (!result.rows) {
-        setResponseStatus(event, 400);
-        return [];
-    }
+	if (!result.rows) {
+		setResponseStatus(event, 400);
+		return [];
+	}
 
-    return result.rows.map((value) => {
-        return {
-            id: value.id as string,
-            name: value.name as string,
-        };
-    });
+	return result.rows.map((value) => {
+		return {
+			id: value.id as string,
+			name: value.name as string,
+		};
+	});
 });
